a16709e827c07f79f298428b6fff1b29449171c1
Joachim Jablon <ewjoachim@gmail.com>
2016-07-13 13:07:06 +0200

    Added initial files and explanations

---
+++ initial_files/README.md
@@ -0,0 +1,10 @@
+# Exercice !
+
+## Les 2 fichiers sont
+
+ - Un extrait d'un relevé à la seconde d'informations issues de capteurs sur un véhicule dont les 25 et 26e colonnes sont les coordonnées GPS
+ - Un module python contenant une fonction qui calcule la distance entre 2 couples de coordonnées GPS
+
+## Ce qu'il faut trouver :
+
+ - La distance totale parcourrue par le véhicule
---
+++ initial_files/distance.py
@@ -0,0 +1,23 @@
+
+from math import cos, sin, radians, atan2, sqrt
+radius = 6371  # km
+
+def distance(origin, destination):
+    """
+    Determine distance between 2 sets of [lat,lon] in km
+    """
+
+    lat1, lon1 =origin[0], origin[1]
+    lat2, lon2 = destination[0], destination[1]
+    dlat = radians(lat2 - lat1)
+    dlon = radians(lon2 - lon1)
+    sindlat = sin(dlat / 2) 
+    sindlon = sin(dlon / 2)
+    a = (sindlat * sindlat +
+         cos(radians(lat1)) *
+         cos(radians(lat2)) * 
+          sindlon * sindlon)
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    d = radius * c
+
+    return d
